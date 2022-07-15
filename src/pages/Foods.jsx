1e5131c7358b19e24b24171a607bcf0cef39b434
rafakasi <rafaelrksilva@gmail.com>
2022-07-15 17:01:58 -0300

    req 13

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import { RecipesContext } from '../context/RecipesContext';
 
 export default function Foods() {
+  const { recipe } = useContext(RecipesContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (recipe.length === 1) {
+      const idFood = recipe[0].idMeal;
+      history.push(`/foods/${idFood}`);
+    }
+  }, [recipe]);
+
   return (
     <div>
       <Header title="Foods" hasSearch />
