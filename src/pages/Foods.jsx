166eec0a053efc1ba28b69bfa1a9ab09e132c6b4
Thales Maciel <thalesmsm12@gmail.com>
2022-07-18 14:32:46 -0300

    muda setRecipe

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -7,18 +7,19 @@ import Footer from '../components/Footer';
 
 export default function Foods() {
   const { recipe, fetchRecipes } = useContext(RecipesContext);
+  const { foods } = recipe;
   const history = useHistory();
 
   useEffect(() => {
-    if (recipe && recipe.length === 1) {
-      const idFood = recipe[0].idMeal;
+    if (foods && foods.length === 1) {
+      const idFood = foods[0].idMeal;
       history.push(`/foods/${idFood}`);
     }
-    if (!recipe) {
+    if (!foods) {
       global.alert(`${''}Sorry, we haven't found any recipes for these filters.`);
       fetchRecipes([]);
     }
-  }, [recipe]);
+  }, [foods]);
 
   function recipeFilterLimit(arr) {
     const limit = 12;
@@ -35,7 +36,7 @@ export default function Foods() {
         <Header title="Foods" hasSearch />
       </div>
       <div>
-        { !!recipe && recipeFilterLimit(recipe).map((recipeF, index) => (
+        { !!foods && recipeFilterLimit(foods).map((recipeF, index) => (
           <FoodCard
             key={ recipeF.idMeal }
             recipeF={ recipeF }
