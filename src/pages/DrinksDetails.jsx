207265739f44b4bfa76fcc578b8600887964153e
rafakasi <rafaelrksilva@gmail.com>
2022-07-19 18:32:41 -0300

    reqs 24-26

--- src/pages/DrinksDetails.jsx
+++ src/pages/DrinksDetails.jsx
@@ -1,13 +1,34 @@
-import React, { useEffect, useContext } from 'react';
-import { RecipesContext } from '../context/RecipesContext';
+import React, { useState, useEffect } from 'react';
+import RecipeDetails from '../components/RecipeDetails';
+import FetchFoods from '../services/FetchFoods';
 
 export default function DrinksDetails() {
-  const { recipe } = useContext(RecipesContext);
+  const [food, setFood] = useState([]);
+  const six = 6;
+
+  const getFood = async () => {
+    const foods = await FetchFoods.fetch12recipes();
+    setFood(foods);
+  };
 
   useEffect(() => {
-  }, [recipe]);
+    getFood();
+  }, []);
 
   return (
-    <h1>Drinks Details</h1>
+    <div>
+      <RecipeDetails />
+      { !!food
+      && food.slice(0, six).map((r, i) => (
+        <div
+          data-testid={ `${i}-recomendation-card` }
+          key={ i }
+        >
+          <img src={ r.strMealThumb } alt="recomendacoes" />
+          <p>{ r.strMeal }</p>
+        </div>
+      ))}
+    </div>
+
   );
 }
